aa48c9567cc29ab84a67bb84fa5e7315ad6f46a4
Rachel Nehmer <rachel@nuanced.dev>
2025-06-03 11:22:39 -0700

    exporting functions to directly invoke analysis and pattern matching

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+export { analyzeFiles } from "./analysis/analyzer";
+export { default as Solver } from "./analysis/solver";
+export { FragmentState } from "./analysis/fragmentstate";
+export { NativeObjectToken } from "./analysis/tokens";
+export { FunctionInfo, ModuleInfo, PackageInfo } from "./analysis/infos";
+
+export { getAPIUsage } from "./patternmatching/apiusage";
+
+export { expand } from "./misc/files";
+
+export { options } from "./options";
